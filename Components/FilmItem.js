616bd3fc2ce20eae7c61423dd74113f3e73ed20a
Redwan <redwan@integral-service.fr>
2020-02-09 15:37:53 +0100

    add film item component to display list of films

---
+++ Components/FilmItem.js
@@ -0,0 +1,84 @@
+// Components/FilmItem.js
+
+import React from 'react'
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import { getImageFromAPI } from '../API/api_conf'
+
+class FilmItem extends React.Component {
+  render() {
+    const film = this.props.film;
+    const displayFilmDetail = this.props.displayFilmDetail
+    return (
+      <TouchableOpacity 
+          style={styles.mainContainer}
+          onPress={() => displayFilmDetail(film.id)}>
+          <Image 
+            style={styles.filmImage}
+            source={{uri: getImageFromAPI(film.poster_path)}}
+          />
+          <View style={styles.filmDescription}>
+            <View style={styles.descriptionHeader}>
+                <Text style={styles.titleText}>{film.title}</Text>
+                <Text style={styles.voteText}>{film.vote_average}</Text>
+            </View>
+            <View style={styles.descriptionContent}>
+                <Text style={styles.descriptionText} numberOfLines={6}>{film.overview}</Text>
+            </View>
+            <View style={styles.descriptionFooter}>
+                <Text style={styles.dateText}>Sorti le {film.release_date}</Text>
+            </View>
+          </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+      height: 190,
+      flexDirection: "row",
+      marginBottom: 2,
+      backgroundColor: "white"
+  },
+  filmImage: {
+        flex: 1,
+        margin: 5
+  },
+  filmDescription: {
+      flex: 2,
+      flexDirection: "column",
+      justifyContent: "space-between",
+      margin: 5
+  },
+  descriptionHeader: {
+      flexDirection: "row",
+      flex: 3
+  },
+  descriptionContent: {
+      flex: 7
+  }, 
+  descriptionFooter: {
+      flex: 1
+  },
+  titleText: {
+      fontWeight: "bold",
+      fontSize: 20,
+      flex: 1,
+      flexWrap: "wrap"
+  },
+  voteText: {
+      fontWeight: "bold",
+      fontSize: 26,
+      color: "#666666"
+  },
+  descriptionText: {
+      fontStyle: 'italic',
+      color: "#666666"
+  },
+  dateText: {
+      textAlign: 'right',
+      fontSize: 14
+  }
+});
+
+export default FilmItem
