249045983b35fe6e5bef832cc9ff92b3b8e7cc33
Redwan <redwan@integral-service.fr>
2020-02-29 18:46:53 +0100

    Begin implement bottom tab navigator (it is working but dammm ugly for now)

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,20 +1,41 @@
 //Navigation/Navigation.js
 
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation'
+import * as React from 'react';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from '@react-navigation/native';
+import {createAppContainer} from 'react-navigation'
 import Search from '../Components/Search';
 import FilmDetail from '../Components/FilmDetail';
+import Favorites from '../Components/Favorites';
 
 const SearchStackNavigator = createStackNavigator({
-    Search: {
-        screen: Search,
-        navigationOptions: {
-            title: "Rechercher"
-        }
-    },
-    FilmDetail: {
-        screen: FilmDetail
-    }
+  Search: {
+      screen: Search,
+      navigationOptions: {
+          title: "Rechercher"
+      }
+  },
+  FilmDetail: {
+      screen: FilmDetail,
+      navigationOptions: {
+          title: "Détails du film"
+      }
+  }
 });
 
-export default createAppContainer(SearchStackNavigator);
+const MoviesTabNavigator = createBottomTabNavigator();
+
+function mainTab() {
+    return(
+        <NavigationContainer>
+            <MoviesTabNavigator.Navigator initialRouteName="Rechercher">
+                <MoviesTabNavigator.Screen name="Rechercher" component={Search} />
+                <MoviesTabNavigator.Screen name="Favoris" component={Favorites} />
+            </MoviesTabNavigator.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default MoviesTabNavigator;
