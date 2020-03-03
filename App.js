04e738cf8b7efeab4b1d631bc34cdd5b9c2f91f4
Redwan <redwan@integral-service.fr>
2020-03-03 14:47:09 +0100

    refactor navigation'

--- App.js
+++ App.js
@@ -4,20 +4,13 @@ import React from 'react'
 import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
-import Search from "./Components/Search";
-import Favorites from "./Components/Favorites";
-import {NavigationContainer} from "@react-navigation/native";
+
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={Store}>
-        <NavigationContainer>
-          <Navigation.Navigator initialRouteName="Rechercher">
-            <Navigation.Screen name="Rechercher" component={Search} />
-            <Navigation.Screen name="Favoris" component={Favorites} />
-          </Navigation.Navigator>
-        </NavigationContainer>
+        <Navigation />
       </Provider>
     )
   }
